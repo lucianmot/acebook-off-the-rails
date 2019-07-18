8de0f968d5f261d55793e92dad5542056c345442
Aleksandra <ajmpawlik@gmail.com>
2019-07-18 11:28:42 +0100

    test: ensure user password cannot be longer than 10 characters

--- spec/features/user_password_is_6_to_10_chars_long_spec.rb
+++ spec/features/user_password_is_6_to_10_chars_long_spec.rb
@@ -10,4 +10,18 @@ RSpec.feature "Sign-up password", type: :feature do
     expect(current_path).to eq("/users")
     expect(page).to have_content("Password is too short (minimum is 6 characters)")
   end
+  scenario "User password is more than 10 characters" do
+    visit "/users/sign_up"
+    fill_in "user_email", with: "example@gmail.com"
+    fill_in "user_password", with: "1234567891011"
+    fill_in "user_password_confirmation", with: "1234567891011"
+    click_button "Sign up"
+    expect(current_path).to eq("/users")
+    expect(page).to have_content("Password is too long (maximum is 10 characters)")
+  end
 end
+# testing password that is too long
+# testing password that is the right minimum_password_length
+# testing the right error message when the password confirmation not filled in
+# tesing no password typed in - right error message received
+# testing not matching passwords
