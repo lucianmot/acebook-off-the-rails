411be0e37b5da0f27bfe79fac3d30fea84a96084
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-18 14:48:20 +0100

    TEST: web_helpers.rb corrected with Rubocop

--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,8 +1,8 @@
 def sign_up
-   visit "/users/sign_in"
-    click_link "Sign up"
-    fill_in("Email", :with => "user@gmail.com")
-    fill_in("Password", :with => "123456")
-    fill_in("Password confirmation", :with => "123456")
-    click_button "Sign up"
+  visit "/users/sign_in"
+  click_link "Sign up"
+  fill_in("Email", :with => "user@gmail.com")
+  fill_in("Password", :with => "123456")
+  fill_in("Password confirmation", :with => "123456")
+  click_button "Sign up"
 end
