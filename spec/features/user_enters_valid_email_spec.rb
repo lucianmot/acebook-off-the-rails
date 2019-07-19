e857d60809dc326745c6ec3ce468c3527d76234e
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-19 14:13:10 +0100

    chore: added a log out link to the index page so we can run features tests

--- spec/features/user_enters_valid_email_spec.rb
+++ spec/features/user_enters_valid_email_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
-RSpec.feature 'Sign-up email', type: :feature do
-  
+RSpec.feature 'Sign-in email', type: :feature do
+  scenario "user enter blank password with valid e-mail" do
+    # sign_up
+    visit("/")
+    click_button "Sign in"
+    fill_in('Email', :with => 'user@gmail.com')
+    click_button "Sign In"
+    expect(page).to have_content("Invalid Email or password.")
+  end
 end
