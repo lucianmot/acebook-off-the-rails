83fd4a5d491cca1dc6f32022dbe1990e77d56ead
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-19 15:56:28 +0100

    TEST: Sign Up Email Unique

--- spec/features/user_email_is_valid_on_sign_up_spec.rb
+++ spec/features/user_email_is_valid_on_sign_up_spec.rb
@@ -11,17 +11,17 @@ RSpec.feature "Sign-up email", type: :feature do
     expect(page).to_not have_content("Welcome! You have signed up successfully.")
   end
 
-  # NEED LOG OUT FEATURE TO ACTIVATE THIST TEST = MERGE WITH LM AND JH BRANCH
-  # scenario "User email is unique" do
-  #   sign_up
-  #   click_button "Sign up"
-  #   fill_in("Email", :with => "user@gmail.com")
-  #   fill_in("Password", :with => "123456")
-  #   fill_in("Password confirmation", :with => "123456")
-  #   click_button "Sign up"
-  #   expect(current_path).to eq("/users")
-  #   expect(page).to have_content("Email has already been taken")
-  # end
+  scenario "User email is unique" do
+    sign_up
+    click_link "Sign out"
+    click_button "Sign up"
+    fill_in("Email", :with => "user@gmail.com")
+    fill_in("Password", :with => "123456")
+    fill_in("Password confirmation", :with => "123456")
+    click_button "Sign up"
+    expect(current_path).to eq("/users")
+    expect(page).to have_content("Email has already been taken")
+  end
 
   scenario "User email is blank but password and password confirmation match" do
     visit "/users/sign_up"
@@ -55,9 +55,3 @@ RSpec.feature "Sign-up email", type: :feature do
     expect(page).to have_content("Password confirmation doesn't match Password")
   end
 end
-
-# email has correct format - done
-# email is not in the database (has to be unique) - IN PROGRESS
-# email field is left blank but password and password confirmation is filled out - done
-# email field, password and password confirmation is left blank - done
-# email field is left blank and password and password confirmation do not match - done
--- spec/features/web_helpers.rb
+++ spec/features/web_helpers.rb
@@ -1,8 +1,14 @@
-def sign_up
+def sign_up(mail="user@gmail.com", pass="123456")
   visit "/users/sign_in"
   click_link "Sign up"
-  fill_in("Email", :with => "user@gmail.com")
-  fill_in("Password", :with => "123456")
-  fill_in("Password confirmation", :with => "123456")
+  fill_in("Email", :with => mail)
+  fill_in("Password", :with => pass)
+  fill_in("Password confirmation", :with => pass)
   click_button "Sign up"
+end
+
+def sign_up_with_sign_out
+   sign_up
+   click_link "Sign out"
+   click_button "Sign in"
 end
