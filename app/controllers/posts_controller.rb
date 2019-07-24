d4c025ec34d1e9a602eac96f460c09ec41b3ce4c
Lucian Mot <lucian.i.mot@gmail.com>
2019-07-24 15:48:19 +0100

    chore: refactor for edit post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,9 +19,9 @@ class PostsController < ApplicationController
     redirect_to user_posts_path
   end
 
-  def edit
-    @post = Post.find(params[:id])
-  end
+  # def edit
+  #   @post = Post.find(params[:id])
+  # end
 
   def update
     @post = Post.find(params[:id])
