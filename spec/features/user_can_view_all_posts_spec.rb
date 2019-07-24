6be20e4c6cf1b1c3980ead369057776e259012a5
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-24 11:58:36 +0100

    REFACTOR: Added helper method create_a_post to Wall feature tests

--- spec/features/user_can_view_all_posts_spec.rb
+++ spec/features/user_can_view_all_posts_spec.rb
@@ -10,9 +10,7 @@ RSpec.feature "View all posts", type: :feature do
   scenario "User can see the posts they have made on the all posts page" do
     sign_up
     click_link "View Your Posts"
-    click_link "New post"
-    fill_in("post_message", with: "Hello")
-    click_button "Submit"
+    create_a_post("Hello")
     click_link "View All Posts"
     expect(current_path).to eq("/posts")
     expect(page).to have_content("Hello")
@@ -21,15 +19,11 @@ RSpec.feature "View all posts", type: :feature do
   scenario "User can see other user's posts as well as their own" do
     sign_up("user@gmail.com")
     click_link "View Your Posts"
-    click_link "New post"
-    fill_in("post_message", with: "I am user 1")
-    click_button "Submit"
+    create_a_post("I am user 1")
     click_link "Sign out"
     sign_up("user2@gmail.com")
     click_link "View Your Posts"
-    click_link "New post"
-    fill_in("post_message", with: "I am user 2")
-    click_button "Submit"
+    create_a_post("I am user 2")
     click_link "View All Posts"
     expect(page).to have_content("I am user 1")
     expect(page).to have_content("I am user 2")
@@ -38,9 +32,7 @@ RSpec.feature "View all posts", type: :feature do
   scenario "User can navigate back to their personal post page after viewing all posts" do
     sign_up
     click_link "View Your Posts"
-    click_link "New post"
-    fill_in("post_message", with: "Hello")
-    click_button "Submit"
+    create_a_post("Hello")
     click_link "View All Posts"
     click_link "View Your Posts"
     expect(current_path).to eq("/users/1/posts")
