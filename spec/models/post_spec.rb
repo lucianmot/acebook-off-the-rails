8fdd5f7e06ac3efe57f30c782a8f1e58605b5c6d
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-25 11:30:46 +0100

    REFACTOR: Changed user_email method in Post modle to delegate :email

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,16 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it "Returns the author of the post's email" do
-    user = User.new(email: "user@gmail.com", id: 1)
-    post = user.posts.new(message: "Hello", user_id: 1)
 
-    expect(post.user_email).to eq "user@gmail.com"
+  before(:each) do
+    @user = User.new(email: "user@gmail.com", id: 1)
   end
 
+  # it "Returns the author of the post's email" do
+  #   post = @user.posts.new(message: "Hello", user_id: 1)
+  #
+  #   expect(post.user_email).to eq "user@gmail.com"
+  # end
+
   it "Returns the formatted date and time a post is created" do
-    user = User.new(email: "user@gmail.com", id: 1)
-    post = user.posts.new(message: "Hello", user_id: 1, created_at: "2019-07-24 11:38:05.223399")
+    post = @user.posts.new(message: "Hello", user_id: 1, created_at: "2019-07-24 11:38:05.223399")
 
     expect(post.formatted_date_time).to eq "24 July 2019 11:38"
   end
