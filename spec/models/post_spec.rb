118ed76be5245e76d7ddd3170418068d145f62c9
Rachel Griffiths <racheljgriffiths@googlemail.com>
2019-07-25 11:02:15 +0100

    TEST: Unit test for user_email method on Post model

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to be }
+  it "Returns the author of the post's email" do
+    user = User.new(email: "user@gmail.com", id: 1)
+    post = user.posts.new(message: "Hello", user_id: 1)
+
+    expect(post.user_email).to eq "user@gmail.com"
+  end
+
+  it "Returns the formatted date and time a post is created" do
+    
+  end
 end
